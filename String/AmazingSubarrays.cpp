c98dbca8ad0232640acf3064b200f9d29bc74973
Tejas Patil <62493951+TejasPatil80178@users.noreply.github.com>
2020-09-02 16:54:23 +0530

    Update AmazingSubarrays.cpp

--- String/AmazingSubarrays.cpp
+++ String/AmazingSubarrays.cpp
@@ -1,24 +1,10 @@
-bool isVowel(char x)
-{
-    if(x=='a'|| x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U')
-    {
-        return 1;
-    }
-    else{
-        return 0;
-    }
-}
-
 int Solution::solve(string s) {
-    int n = s.size();
-    int k = 0;
-    for(int i=0;i<n;i++)
-    {
-        if(isVowel(s[i]))
-        {
-            k = k+(n-i);
-        }
-    }
-    
-    return k%10003;
+    int sum = 0;
+    int n = s.length();
+	for(int i=0;i<n;i++){
+		if(s[i]=='a'||s[i]=='e'||s[i]=='o'||s[i]=='i'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
+			sum = ((sum%10003) + (n-i)%10003)%10003;
+		}
+	}
+	return sum%10003;
 }
