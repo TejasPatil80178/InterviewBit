700cb0bee6a8be5cc8197b956735a2c371e90c44
Tejas Patil <62493951+TejasPatil80178@users.noreply.github.com>
2020-09-02 16:08:57 +0530

    Update PalindromeString.cpp

--- String/PalindromeString.cpp
+++ String/PalindromeString.cpp
@@ -1,37 +1,25 @@
-bool isNotChar(char a)
-{
-	if((97<=a && 122>=a) || (65<=a && 90>=a) || (48<=a && 57>=a))
-	{
-		return 0;
+bool isan(char c){
+	if(c>='a' && c<='z' || c>='A' && c<='Z' || c>='0' && c<='9'){
+		return 1;
 	}
 	else{
-		return 1;
+		return 0;
 	}
 }
 
 int Solution::isPalindrome(string s) {
-    int i = 0;
-	int j = s.length()-1;
-	while(i<=j)
-	{
-		if(isNotChar(s[i]))
-		{
-		    //cout<<s[i]<<endl;
-			i++;
-		}
-		else if(isNotChar(s[j]))
-		{
-		    //cout<<s[j]<<endl;
-			j--;
-		}
-		else if(s[i]==s[j] || abs((int)s[i] - (int)s[j])==32)
-		{
-			i++;
-			j--;
-		}
-		else{
-			return 0;
+    string a;
+	int n = s.length();
+	for(int i=0;i<n;i++){
+		if(isan(s[i])){
+			if(s[i]>='A' && s[i]<='Z'){
+				s[i] = s[i] + 32;
+			}
+			a+=s[i];
 		}
 	}
-	return 1;
+	string b = a;
+	reverse(b.begin(),b.end());
+	if(a==b) return 1;
+	else return 0;
 }
