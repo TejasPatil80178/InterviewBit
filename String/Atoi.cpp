e904887032152525bc406ef0db2df6714466db71
Tejas Patil <62493951+TejasPatil80178@users.noreply.github.com>
2020-09-02 20:20:40 +0530

    Update Atoi.cpp

--- String/Atoi.cpp
+++ String/Atoi.cpp
@@ -1,33 +1,33 @@
 int Solution::atoi(const string s) {
-   	int n = s.length();
+    int n = s.length();
 	int sign = 1;
-	int base = 0;
 	int i = 0;
-	while(s[i]==' ')
-	{
+	while(s[i]==' '){
 		i++;
 	}
-	if(s[i]=='-' || s[i]=='+')
-	{
+	if(s[i]=='-' || s[i]=='+'){
 		if(s[i]=='-') sign = -1;
 		i++;
 	}
-	while(s[i]-'0'>=0 && s[i]-'0'<=9)
-	{
-		if(base > INT_MAX/10 || base==INT_MAX/10 && s[i]-'0'==7)
-		{
-			if(sign==-1)
-			{
-				return INT_MIN;
-			}
-			else{
-				return INT_MAX;
+	string a;
+	for(;i<n;i++){
+		if(s[i]>='0' && s[i]<='9'){
+			a+=s[i];
+			if(stoll(a)>INT_MAX){
+				if(sign == -1) return INT_MIN;
+				else return INT_MAX;
 			}
 		}
-		
-		base = (10*base) + (s[i]-'0');
-		i++;
+		else{
+			break;
+		}
 	}
-	
-	return base*sign;
+	if(a.empty()) a+='0';
+	if(stoll(a)>INT_MAX){
+		if(sign==-1) return INT_MIN;
+		else return INT_MAX;
+	}
+	else{
+	    return (sign*stoi(a));
+	}
 }
